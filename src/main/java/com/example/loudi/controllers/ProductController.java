1747beea9826d772f83a7d63adeaf85983945a90
Loudsilence <loudsilencesec@gmail.com>
2023-09-12 01:59:37 +0500

    Add product

--- src/main/java/com/example/loudi/controllers/ProductController.java
+++ src/main/java/com/example/loudi/controllers/ProductController.java
@@ -1,10 +1,14 @@
 package com.example.loudi.controllers;
 
+import com.example.loudi.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
 
     @GetMapping("/")
     public String products() {
---
+++ src/main/java/com/example/loudi/services/ProductService.java
@@ -0,0 +1,31 @@
+package com.example.loudi.services;
+
+import com.example.loudi.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> products = new ArrayList<>();
+    private long ID = 0;
+
+    {
+        products.add(new Product(++ID ,"PS5", "Simpel", 67000, "Krasnoyrsk", "Sony"));
+        products.add(new Product(++ID ,"Imphone 9", "Deckription", 90000, "Moskov", "Apple"));
+    }
+
+    public List<Product> list(){
+        return products;
+    }
+    public void saveProduct(Product product){
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProdurct(Long id){
+        products.removeIf(product -> product.getId().equals(id));
+    }
+}
